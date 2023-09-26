ad3e63b6f3cda129f1e1844f772fd247a53733a9
Jan Panol <102944499+j4np4n0l@users.noreply.github.com>
2023-09-26 14:12:14 +0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,62 +1,77 @@
-headerHeightChange();
-imgAutoresize();
-window.addEventListener('resize', headerHeightChange);
-window.addEventListener('resize', imgAutoresize);
+// Data attribute for adding inline style for user-defined padding and margin
+// Custom inline style (padding="value", margin="value") 
+// const inlineStyle = (element, data_attr1, data_attr2) => {
+// 	const elements = document.querySelectorAll(element);
+	
+// 	let element_count = 0;
 
-// img-cover class
-document.addEventListener('DOMContentLoaded', () => {
-  for (const img of document.querySelectorAll('.img-cover img')) {
-    img.parentElement.style.backgroundImage = `url(${img.src})`;
-  }
-})
+// 	elements.forEach((element_tag) => {
+// 		element_count++;
 
-// Data attribute for adding style for user-defined padding and margin
-elementStyle('section', 'padding', 'margin');
-elementStyle('.row', 'padding', 'margin');
-elementStyle('.column', 'padding', 'margin');
+// 		padding = element_tag.getAttribute(data_attr1);
+// 		margin = element_tag.getAttribute(data_attr2);
 
-function elementStyle(element, data_attr1, data_attr2) {
-	const elements = document.querySelectorAll(element);
+// 		if (padding) {
+// 			element_tag.style.padding = padding;	
+// 		}
 
-	elements.forEach((element_tag) => {
-		padding = element_tag.getAttribute(data_attr1);
+// 		if (margin) {
+// 			element_tag.style.margin = margin;	
+// 		}
 
-		if (padding) {
-			element_tag.style.padding = padding;
-		}
+// 	});
+// }
 
-		margin = element_tag.getAttribute(data_attr2);
+const inlineStyle = () => {
+	const elements = document.querySelectorAll( 'body *' );
 
-		if (margin) {
-			element_tag.style.margin = margin;
+	element_array = 0;
+	
+	elements.forEach((element) => {
+		element_array++;
+
+		if (element.hasAttributes()) {
+			let output = "";
+			for (const attr of element.attributes) {
+	        	output += `${attr.name} -> ${attr.value}\n`;	        	
+	        	element.style.setProperty(attr.name, attr.value);
+	        }
 		}
 	});
 }
 
-// Fixed header & Fullpage hero
-function headerHeightChange() {
+
+
+
+// Fixed header (header.fixed) & Fullpage hero (#hero.fullpage)
+const headerHeightChange = () => {
 	const main = document.querySelector('main');
 	const header = document.querySelector('header');	
 	const nav_menu = document.querySelector('nav');
 	const hero = document.querySelector('.fullpage');
-	let header_height = header.offsetHeight;
+	const header_height = header.offsetHeight;
 
 	if (header.classList.contains('fixed') ) {
-		main.style.marginTop = header_height + 'px';
+		main.style.marginTop = `${header_height}px`;
 	} else {
 		main.style.marginTop = '0';
 	}
 
-	hero.style.height = 'calc(100vh - ' + header_height + 'px)';
-	nav_menu.style.top = header_height + 'px';
+	hero.style.height = `calc(100vh - ${header_height}px)`;
+	nav_menu.style.top = `${header_height}px`;
 }
 
-// Auto-resize image when width exceeded to the size of the screen
-function imgAutoresize() {
+// If .img-cover added to .img-container (.image-container.img-cover),
+// it will convert the image as background of the container and
+// auto-resize image when width exceeded to the size of the screen
+const imgAutoresize = () => {
 	const imgs = document.querySelectorAll('.img-cover img');
-	let container_width = screen.width * 0.9;
+	const container_width = screen.width * 0.9;
 
 	imgs.forEach((img) => {
+		//convert the image as a background of the .img-container
+		img.parentElement.style.backgroundImage = `url(${img.src})`;
+
 		let img_width = img.naturalWidth;
 		if (img_width >= container_width) {
 			img.style.width = '100%';
@@ -65,3 +80,14 @@ function imgAutoresize() {
 		}
 	});
 }
+
+const clog = (logtext) => {
+	console.log(logtext);
+}
+
+window.addEventListener('DOMContentLoaded', headerHeightChange);
+window.addEventListener('DOMContentLoaded', imgAutoresize);
+window.addEventListener('DOMContentLoaded', inlineStyle);
+
+window.addEventListener('resize', headerHeightChange);
+window.addEventListener('resize', imgAutoresize);
